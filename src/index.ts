5912424efd2dc104d76e346266753517c8992ef2
David Khourshid <davidkpiano@gmail.com>
2018-08-07 16:39:40 -0400

    Adding comments to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 
+// Creates a new Express app instance
 const app = express();
 
+// Configures the http://localhost:5000/ route to send a text response
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
+// Starts the app on port 5000, then calls the callback when
+// the app successfully starts.
 app.listen(5000, () => {
     console.log('Listening on port 5000: http://localhost:5000');
 });
