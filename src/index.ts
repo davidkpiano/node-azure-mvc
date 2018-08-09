8e210a7f9ec8d9b0ce80dc3c3603dd3d585b5006
David Khourshid <davidkpiano@gmail.com>
2018-08-09 16:09:50 -0400

    Connecting to DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import express from 'express';
 import moviesRouter from './routers/MoviesRouter';
 import * as path from 'path';
+import { connect } from 'mongoose';
+
+// Connect the database
+// We'll remove the hardcoded URL later.
+const mongoUrl = 'mongodb://127.0.0.1:27017/moviesapp';
+connect(mongoUrl)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.log('MongoDB connection error.');
+    });
 
 // Creates a new Express app instance
 const app = express();
---
+++ src/models/Movie.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose';
+
+// Create Movie schema
+const MovieSchema = new Schema({
+    title: String,
+    releaseDate: Date,
+    genre: String,
+    price: Number
+});
+
+// Create Movie model
+const Movie = model('Movie', MovieSchema);
+
+export default Movie;
