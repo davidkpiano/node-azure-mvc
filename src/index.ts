677e279d0a77de120dc9618f6cf28e9a76d5cc05
David Khourshid <davidkpiano@gmail.com>
2018-08-20 14:58:09 -0400

    Adding server.js

--- src/index.ts
+++ src/index.ts
@@ -34,10 +34,4 @@ app.use('/', (req, res) => res.send('Hello world!'));
 // Handles /movies routes
 app.use('/movies', moviesRouter);
 
-const port = process.env.PORT || 5000;
-
-// Starts the app on the configured port, then calls the callback when
-// the app successfully starts.
-app.listen(port, () => {
-    console.log(`Listening on port ${port}: http://localhost:${port}`);
-});
+export { app };
