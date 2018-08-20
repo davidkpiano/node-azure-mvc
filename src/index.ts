cef53432927b7db29d2a01943fcbc87d51c8a762
David Khourshid <davidkpiano@gmail.com>
2018-08-20 16:03:55 -0400

    Changes

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.engine('jsx', require('express-react-views').createEngine());
 // Parses JSON in body
 app.use(express.json());
 
-app.use('/', (req, res) => res.send('Hello world!'));
+app.use('/', (req, res) => res.send('Hello 33world!'));
 
 // Handles /movies routes
 app.use('/movies', moviesRouter);
