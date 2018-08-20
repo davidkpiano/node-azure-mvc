5422007745e63b8da8cccb67875aa2f2e370d56f
David Khourshid <davidkpiano@gmail.com>
2018-08-20 15:32:15 -0400

    Commengting out database connection

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,14 @@ dotenv.config();
 
 // Connect the database
 // We'll remove the hardcoded URL later.
-const mongoUrl = 'mongodb://127.0.0.1:27017/moviesapp';
-connect(mongoUrl)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(err => {
-        console.log('MongoDB connection error.');
-    });
+// const mongoUrl = 'mongodb://127.0.0.1:27017/moviesapp';
+// connect(mongoUrl)
+//     .then(() => {
+//         console.log('Connected to MongoDB');
+//     })
+//     .catch(err => {
+//         console.log('MongoDB connection error.');
+//     });
 
 // Creates a new Express app instance
 const app = express();
