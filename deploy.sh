a12d3a9469d2b238e7ff6add384bff6b9ed2e1a0
David Khourshid <davidkpiano@gmail.com>
2018-08-20 18:25:46 -0400

    Adding start script to deploy script

--- deploy.sh
+++ deploy.sh
@@ -113,6 +113,7 @@ selectNodeVersion
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   eval $NPM_CMD install --production
+  eval $NPM_CMD run build-ts
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
